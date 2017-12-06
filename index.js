5ec642b1ff1220c810259e236a393158eb5984b1
melissames <melissa.sames@gmail.com>
2017-12-06 19:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
-  return kittens.unshift(name)
+  return kittens.shift(name)
 }
